9c3a764e2e9316e7ebe4bb203da18fd61a9b3057
Jose Villa <villa1997.jv@gmail.com>
2025-01-22 14:00:34 -0600

    new

--- Program.cs
+++ Program.cs
@@ -1,37 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Wk1Ex2
+﻿namespace Wk1Ex2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Type your first name and press enter
+            //declaration
+            string firstName = "";
+            string lastName = "";
+            int age = "0";
+            string color = "";
+
+            //print out the prompt to ask for first name input
             Console.WriteLine("What is your first name?");
-            //Create a string variable and get user input
-            string firstName = Console.ReadLine();
+            //collect the user input for first name
+            firstName = Console.ReadLine();
 
-            //Type your last name
+            //print out the prompt to ask for last name input
             Console.WriteLine("What is your last name?");
-            //Create a string variable and get user input
-            string lastName = Console.ReadLine();
+            //collect the user input for last name
+            lastName = Console.ReadLine();
 
-            //Type your age
+            //print out the prompt to ask for your age
             Console.WriteLine("What is your age?");
-            //Crate a string for your age
-            int age = Convert.ToInt32(Console.ReadLine());
+            //collect the user input for their age
+            age = Convert.ToInt32(Console.ReadLine());
 
-            //Type your favorite color
+            //print out the prompt to ask for your favorite color input
             Console.WriteLine("What is your favorite color?");
-            //Create a string for your favorite color
-            string color = Console.ReadLine();
+            //collect the user input for your favorite color
+            color = Console.ReadLine();
 
             //Output the information that was typed in one sentence
             Console.WriteLine("Hello " + firstName + " " + lastName + "! You are " + age + " years old and your favorite color is " + color + ".");
-        }
+
+			Console.WriteLine($"Hello {firstName} {lastName}! You are {age} years old and your favorite color is {color}.);
+		}
     }
 }
